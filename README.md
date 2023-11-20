68265c1fb0f83b01a501dad1f3144cb7eed55f70
Isaquehg <isaqueh.goncalves@gmail.com>
2023-11-20 12:17:59 -0300

    v1.0 OK

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # video-editor
-Video Editor usign Python
+Video Editor usign Python, with [MoviePy](https://zulko.github.io/moviepy/index.html) and []
 
 ## Table of Contents
 - [About](#about)
 - [Setup](#setup)
 - [Functionalities](#functionalities)
+  - [Clip Video](#clip-video)
+  - [Brightness](#brightness)
+  - [Contrast](#contrast)
+  - [Volume](#volume)
+  - [Speed](#speed)
+  - [Invert Color](#invert-color)
+  - [Paiting effect](#paiting-effect)
 - [Results](#results)
 
 ## About
+This project is developed for the Computer Graphics and Multimedia lectures (C209). It provides a simple video editor implemented in Python using the MoviePy library. The editor offers various functionalities to clip, modify brightness, contrast, volume, speed, invert colors, and apply a painting effect to videos.
+
 
 ## Setup
 1. Create Virtual Enviroment and activate it
@@ -54,11 +63,28 @@ Video Editor usign Python
 
 ## Functionalities
 ### Clip Video
-### Modify Volume
-### Apply Paiting effect
-### Modify Contrast
-### Modify Speed
-### Modify Brightness
+The `clip_video` function allows you to trim a video by specifying the start and end times.
+
+### Brightness
+The `modify_brightness` function adjusts the brightness of the video using the `gamma_corr` effect in MoviePy.
+
+### Contrast
+The `modify_contrast` function changes the contrast of the video using the `lum_contrast` effect in MoviePy.
+
+### Volume
+The `edit_volume` function modifies the volume of the video by applying a volume factor.
+
+### Speed
+The `modify_speed` function alters the speed of the video by multiplying it by a speed factor.
+
 ### Invert Color
+The `invert_color` function inverts the colors of the video using the `invert_colors` effect in MoviePy.
+
+### Painting Effect
+The `painting_effect` function transforms the video into a painting using the `painting` effect in MoviePy.
+
+### Chrome Key
+The `chroma_key` function applies an image background to replace a given RGB value in the original video
 
 ## Results
+The result from the modifications are saved in the `/results` folder
--- editor.py
+++ editor.py
@@ -1,65 +1,81 @@
 import moviepy.editor as mpe
-import numpy as np
 
 class Editor():
     def __init__(self, path_to_video: str) -> None:
         self.video = mpe.VideoFileClip(path_to_video) # .mp4 video
         self.audio = self.video.audio
+        self.duration = self.video.duration
 
-        print('Tamanho:', self.video.size) ## ou video.h e video.w
+        print('Size:', self.video.size) ## or video.h and video.w
         print('FPS:', self.video.fps)
-        print('Duração:', self.video.duration, 'segundos')
-        print('Número de frames:', self.video.reader.nframes)
-        print('Taxa de amostragem:', self.audio.fps, 'Hz')
+        print('Duration:', self.video.duration, 'seconds')
+        print('Amount of Frames:', self.video.reader.nframes)
+        #print('Sampling Rate:', self.audio.fps, 'Hz')
 
     def clip_video(self, start_time, end_time):
         self.video = self.video.subclip(start_time, end_time)
+        self.duration = self.video.duration
 
-    def edit_volume(self, volume_factor):
+    def volume(self, volume_factor):
         self.audio = self.audio.volumex(volume_factor)
 
-    def paiting_effect(self, factor):
-        self.video = self.video.fx(mpe.vfx.painting, factor)
-
-    def modify_brightness(self, factor):
+    def brightness(self, factor):
         self.video = self.video.fx(mpe.vfx.colorx, factor)
 
-    def modify_contrast(self, factor):
-        self.video = self.video.fx(mpe.vfx.lum_contrast, lum=factor)
+    def contrast(self, factor):
+        self.video = self.video.fx(mpe.vfx.lum_contrast, contrast=factor)
 
-    def modify_speed(self, factor):
+    def speed(self, factor):
         self.video = self.video.fx(mpe.vfx.speedx, factor)
+        self.duration = self.video.duration
 
     def invert_color(self):
         # Inverter as cores do vídeo
         self.video = self.video.fx(mpe.vfx.invert_colors)
 
+    def paiting_effect(self, factor):
+        self.video = self.video.fx(mpe.vfx.painting, factor)
+
+    def chroma_key(self, color_rgb, background_image_path):
+        background = mpe.ImageClip(background_image_path)
+
+        masked_clip = self.video.fx(mpe.vfx.mask_color, color=color_rgb, thr=100, s=5)
+
+        self.video = mpe.CompositeVideoClip([
+            background,
+            masked_clip
+        ]).set_duration(self.duration)
+
     def save_video(self, output_path='./results/modified_video.mp4'):
-        self.video.write_videofile(output_path)
+        self.video.write_videofile(output_path, fps=24)
 
-# Exemplo de uso
-video_editor = Editor("./videos/bigbuckbunny.mp4")
+# Usage Example
+video_editor = Editor("./videos/windmill.mp4")
+#video_editor = Editor("./videos/bigbuckbunny.mp4") -> use this one to test the volume modifications
 
-# Clipando o vídeo
+# Clipping the video
 video_editor.clip_video(3, 5)
 
-# Modificando o volume
-video_editor.edit_volume(1.5)
+# Modifying the Volume
+#video_editor.volume(1.5)
+
+# Modifying Contrast
+video_editor.contrast(1.5)
+
+# Modifying Video Speed
+video_editor.speed(1.5)
+
+# Modifying Brightness
+video_editor.brightness(0.7)
+
+# Inverting Colours
+video_editor.invert_color()
 
 # Efeito de pintura
 video_editor.paiting_effect(1.3)
 
-# Modificando Contraste
-video_editor.modify_contrast(1.2)
+# Applying Chroma Key
+video_editor.chroma_key([4,179,68], "images/nature_grass.png")
 
-# Modifica Velocidade do Vídeo
-video_editor.modify_speed(1.5)
-
-# Modificando Brilho
-video_editor.modify_brightness(0.7)
-
-# Invertendo cores
-video_editor.invert_color()
-
-# Salvando o vídeo modificado
+# Saving Video to /results
 video_editor.save_video()
